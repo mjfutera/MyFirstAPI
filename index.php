2aaafe615288e69a4a96b704ef7a534ccd8fd3fc
Michał Futera <100314711+mjfutera@users.noreply.github.com>
2022-12-28 12:00:07 +0100

    Added random quote

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     $database = 'database.db';
 
     $result = array();
-    $result['API_Owner'] = 'Michal Futera';
-    $result['API_Owner_Link'] = 'https://linktr.ee/mjfutera';
+    $result['api_owner'] = 'Michal Futera';
+    $result['api_owner_link'] = 'https://linktr.ee/mjfutera';
 
     if($url[2]=='joke') {
         $sql = 'SELECT joke FROM randomJoke ORDER BY RANDOM() LIMIT 1';
@@ -21,10 +21,15 @@
         $sql = 'SELECT fact FROM randomFact ORDER BY RANDOM() LIMIT 1';
         $result['fact'] = connectSQLite($sql, $database)[0]['fact'];
         echo json_encode($result);
+    } else if ($url[2]=='quote') {
+        $sql = 'SELECT quote, author FROM randomQuote ORDER BY RANDOM() LIMIT 1';
+        $result['quote'] = connectSQLite($sql, $database)[0];
+        echo json_encode($result);
     } else {
         $result['message'] = 'Choose one random thing';
         $result['links']['joke'] = 'https://api.michalfutera.pro/random/joke';
         $result['links']['fact'] = 'https://api.michalfutera.pro/random/fact';
+        $result['links']['quote'] = 'https://api.michalfutera.pro/random/quote';
         echo json_encode($result);
     }
         
