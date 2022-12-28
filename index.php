36a9c418559954abc9c55d478a5a01d1044de130
Michał Futera <100314711+mjfutera@users.noreply.github.com>
2022-12-28 06:21:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,9 @@
         $result['fact'] = connectSQLite($sql, $database)[0]['fact'];
         echo json_encode($result);
     } else {
-        $result['message'] = 'Choose joke or facts';
+        $result['message'] = 'Choose one random thing';
+        $result['links']['joke'] = 'https://api.michalfutera.pro/random/joke';
+        $result['links']['fact'] = 'https://api.michalfutera.pro/random/fact';
         echo json_encode($result);
     }
         
